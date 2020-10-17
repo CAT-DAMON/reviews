fe27db23c07d9e02c04b98ed910b3bb8893d8d4c
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-17 13:30:11 -0700

    Run Postgresql COPY query via script;
    Also, added in Timer which is a constructor function with methods
    to create a stopwatch easily for inline testing

--- point_in_time_v1/timer.js
+++ point_in_time_v1/timer.js
@@ -1,14 +1,21 @@
 
-
 class Timer {
   constructor(props) {
-    super(props)
     this.moment = () => new Date().getTime() / 1000;
   }
-
-  this.start = (interval) => {
+  start = (interval) => {
     if (interval === 'seconds') {
       return this.moment()
     }
-  }
+  };
+  end = (start, interval) => {
+    if (interval === 'seconds') {
+      let end = this.moment();
+      return `FINISHED IN ${Math.floor((end - start) / 60)} MIN ${Math.floor((end  - start) % 60)} SECONDS`
+    }
+  };
 }
+
+module.exports = {Timer}
+
+
--- server/postgresql/psqlBulkLoad.js
+++ server/postgresql/psqlBulkLoad.js
@@ -1,4 +1,5 @@
-
+const {Timer} = require('../../point_in_time_v1/timer.js');
+const StopWatch = new Timer()
 const { Client } = require('pg');
 const client = new Client({
   user: 'username',
@@ -10,28 +11,16 @@ const client = new Client({
 client.connect();
 const headings = 'userId, userName, userThumb, createdAt, rating, body, itemId, itemName, itemThumb, storeId, imageURL, helpful';
 const file = '/Users/jasedinardo/SDC/Reviews/server/database/sampleData1.csv';
-var count = 1;
-const pointInTime = (interval) => {
-  if (interval === 'seconds') {
-    let time = new Date();
-    let moment = time.getTime() / 1000;
-    return moment;
-  }
-}
-const howLongItTook = (start, end) => {
-  return `FINISHED IN ${Math.floor((end - start) / 60)} MIN ${Math.floor((end  - start) % 60)} SECONDS`
-}
+var count = 2;
 
 const bulkLoad = (schema, mockData) => {
-  var start = pointInTime('seconds');
+  var start = StopWatch.start('seconds');
   const file = `/Users/jasedinardo/SDC/Reviews/server/database/sampleData${count}.csv`;
   client.query(`COPY reviews(${headings}) FROM '${file}' WITH DELIMITER ',';`, (err, res) => {
     if (err) {
       console.error(err, 'POSTGRES BULK LOAD')
-    } else {
-      let end = pointInTime('seconds');
-      console.log(howLongItTook(start, end))
     }
+    console.log(StopWatch.end(start, 'seconds'))
   });
 };
 bulkLoad()
