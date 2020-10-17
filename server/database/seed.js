96f8c96e1b80d926b15a8f8a37891fb44f1358e2
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-16 19:49:11 -0700

    Be able to connect and load 25M in each database;
    Files are not the focus at this point, as of now copy and paste
    the COPY query into the cassandra shell and run

--- server/database/seed.js
+++ server/database/seed.js
@@ -2,68 +2,35 @@ const faker = require('faker');
 const fake = require('./birds.js');
 const fs = require('fs');
 const path = require('path');
-const file = path.join(__dirname, 'psqlData.csv');
+const file = path.join(__dirname, 'sampleData3.csv');
 const writeReviews = fs.createWriteStream(file, {flags: 'a'});
 
 const seed = () => {
-  let birdCount = 0
-  let revIMG = 0;
-  let results = [];
-  for (let s = 0; s < 10000; s++) {
-    for (let d = 0; d < 100; d++) {
-      let itemId = d;
-      for (let c = 0; c < 5; c++) {
+  for (let s = 0; s < 10000; s++) {  // 10000 stores
+    for (let d = 0; d < 100; d++) {  // 100 items
+      for (let c = 0; c < 5; c++) {  // 5 reviews per item === 5M reviews
+        let itemId = d;
         let date = new Date(faker.date.past(6)).toUTCString().split(',').slice(1);
-        if (birdCount === 27) {
-          birdCount = 0;
-        }
-        if (revIMG === 15) {
-          revIMG = 0;
-        }
         let mockData = `${faker.internet.userName()},${fake.names[Math.floor(Math.random() * 650)]},${faker.image.avatar()},${date},${Math.ceil(Math.random() * 5)},${faker.lorem.sentences()},${itemId},${fake.productNames[itemId]},${faker.image.imageUrl()},${s},${faker.image.imageUrl()},${Math.floor(Math.random() * 20)}\n`;
-        birdCount++;
-        revIMG++;
         writeReviews.write(mockData, (err) => {
           if (err) {
             console.error(err);
           }
-        })
+        });
       }
     }
   }
 }
 
 var controller = (limit) => {
-  var pacer = setImmediate(() => {
+  var pacer = setImmediate(() => {  // using setImmediate to invoke garbage collection between calls
     seed();
-    console.log(limit * 10000000)
-    if (limit < 5) {
+    console.log(limit * 5000000) // 1,000,000 reviews added everytime it is reached this point
+    if (limit < 1) {
       controller(++limit);
     } else {
       return;
     }
   });
 }
-
 controller(1);
-// controller(200);
-// controller(300);
-// controller(400);
-// controller(500);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// module.exports.mockData = mockData;
