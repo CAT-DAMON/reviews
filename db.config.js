ca1d5d999181714890770190b83e3e4a5d44f937
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-22 16:21:30 -0700

    Update stress test

--- db.config.js
+++ db.config.js
@@ -12,7 +12,6 @@ module.exports.psql = {
 FROM '/Users/jasedinardo/SDC/Reviews/server/database/pg_data1.csv'
 WITH DELIMITER ',';`,
   insert: `INSERT INTO reviews(id, storeId, itemId, createdAt, helpful, userId, userName, userThumb, rating, body, itemName, itemThumb, imageURL) VALUES (200000000, 200000,0,'30 Nov 2018 12:44:13 GMT',14,'Berneice_Marquardt96','Dr. Guadalupe Gutmann','https://s3.amazonaws.com/uifaces/faces/twitter/raquelwilson/128.jpg',5,'Reprehenderit animi atque quaerat eligendi. Dicta et ad vel beatae corporis quo.','Incredible Cotton Salad - Intelligent- Clothing','http://placeimg.com/640/480','http://placeimg.com/640/480') RETURNING *;`
-
 };
 
 module.exports.cass = {
