280d1e20777f31f8e9ed65ca6744379314f2d149
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-23 08:57:55 -0700

    Quick save

--- server/postgresql/psql.js
+++ server/postgresql/psql.js
@@ -47,7 +47,7 @@ const getItems = (storeId, itemId, order = 'createdAt') => {
 
 const getStore = (storeId, order = 'createdAt') => {
   const start = StopWatch.start('milliseconds');
-  return client.query(`SELECT * FROM reviews WHERE storeId = ${storeId} ORDER BY ${order} DESC`)
+  return client.query(`SELECT * FROM reviews WHERE storeId = ${storeId} ORDER BY ${order} DESC LIMIT 300`)
   .then((store) => {
     StopWatch.end(start, 'getStore')();
     return store;
@@ -103,7 +103,7 @@ const wipeTable = (tableName) => {
   if (tableIsDirty) {
     wipeTable('reviews');
   };
-});
+})();
 
 module.exports = {
   addReview,
